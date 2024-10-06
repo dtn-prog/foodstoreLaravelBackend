d46f52c32f5f8ad5768de38353c29ebe4d360204
dtn-prog <dtncode@gmail.com>
2024-10-06 21:42:43 +0700

    seed admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin'),
+            'phone'=>'0989672531',
+            'role'=>'admin',
+            'lat'=>'1',
+            'long'=>'1'
+        ]);
     }
 }
