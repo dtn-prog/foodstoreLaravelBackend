ca4d07f404b788840c92e3a1ac943633948fbbe1
dtn-prog <dtncode@gmail.com>
2025-01-06 15:43:18 +0700

    remove comment

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone')->unique();
-            // $table->enum('role', ['admin','customer']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
