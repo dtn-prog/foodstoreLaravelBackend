d01763f6d2305e0ea79d870763b8746fd6b0b654
dtn-prog <dtncode@gmail.com>
2025-01-06 21:45:39 +0700

    aurhosize roles

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -3,18 +3,24 @@
 @section('content')
 <div class="container mx-auto px-4">
     <h1 class="text-2xl font-bold my-4">Roles</h1>
-    <a href="{{ route('roles.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Create Role</a>
+    @can('create roles')
+        <a href="{{ route('roles.create') }}" class="bg-blue-500 text-white px-4 py-2 rounded">Create Role</a>
+    @endcan
     <ul class="mt-4">
         @foreach ($roles as $role)
             <li class="flex justify-between items-center border-b py-2">
                 <span class="text-lg">{{ $role->name }}</span>
                 <div>
-                    <a href="{{ route('roles.edit', $role) }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Edit</a>
-                    <form action="{{ route('roles.destroy', $role) }}" method="POST" style="display:inline;" onsubmit="return confirmDelete(event);">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 ml-4">Delete</button>
-                    </form>
+                    @can('edit roles')
+                        <a href="{{ route('roles.edit', $role) }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Edit</a>
+                    @endcan
+                    @can('delete roles')
+                        <form action="{{ route('roles.destroy', $role) }}" method="POST" style="display:inline;" onsubmit="return confirmDelete(event);">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 ml-4">Delete</button>
+                        </form>
+                    @endcan
                 </div>
             </li>
         @endforeach
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,12 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(RoleController::class)->prefix('roles')->group(function () {
-    Route::get('', 'index')->name('roles.index');
-    Route::get('create', 'create')->name('roles.create');
-    Route::post('', 'store')->name('roles.store');
-    Route::get('{role}/edit', 'edit')->name('roles.edit');
-    Route::put('{role}', 'update')->name('roles.update');
-    Route::delete('{role}', 'destroy')->name('roles.destroy');
+    Route::get('', 'index')->name('roles.index')->can('view roles');
+    Route::get('create', 'create')->name('roles.create')->can('create roles');
+    Route::post('', 'store')->name('roles.store')->can('create roles');
+    Route::get('{role}/edit', 'edit')->name('roles.edit')->can('edit roles');
+    Route::put('{role}', 'update')->name('roles.update')->can('edit roles');
+    Route::delete('{role}', 'destroy')->name('roles.destroy')->can('delete roles');
 });
 
 
