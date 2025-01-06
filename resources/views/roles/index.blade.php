87be75d05f9ec8475e9cdf01931a76b6e836f86c
dtn-prog <dtncode@gmail.com>
2025-01-06 18:55:41 +0700

    pop up for delete

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -9,15 +9,25 @@
             <li class="flex justify-between items-center border-b py-2">
                 <span class="text-lg">{{ $role->name }}</span>
                 <div>
-                    <a href="{{ route('roles.edit', $role) }}" class="text-blue-600 hover:underline">Edit</a>
-                    <form action="{{ route('roles.destroy', $role) }}" method="POST" style="display:inline;">
+                    <a href="{{ route('roles.edit', $role) }}" class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700">Edit</a>
+                    <form action="{{ route('roles.destroy', $role) }}" method="POST" style="display:inline;" onsubmit="return confirmDelete(event);">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="text-red-600 hover:underline ml-4">Delete</button>
+                        <button type="submit" class="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700 ml-4">Delete</button>
                     </form>
                 </div>
             </li>
         @endforeach
     </ul>
 </div>
+
+<script>
+    function confirmDelete(event) {
+        const confirmed = confirm("Are you sure you want to delete this role?");
+        if (!confirmed) {
+            event.preventDefault(); // Prevent the form from submitting
+        }
+        return confirmed; // Return true to allow form submission
+    }
+</script>
 @endsection
