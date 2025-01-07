bdaa46d0356c6e675aa4caa9612638b419b42803
dtn-prog <dtncode@gmail.com>
2025-01-07 11:12:21 +0700

    otp controllers

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\CatController;
 use App\Http\Controllers\Api\LocationController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\OtpController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -12,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    // Route to send OTP
+    Route::post('send-otp', [OtpController::class, 'sendOtp'])->name('api.send.otp');
+
+    // Route to verify OTP
+    Route::post('verify-otp', [OtpController::class, 'verifyOtp'])->name('api.verify.otp');
 });
 
 Route::post('register', [RegisterController::class, 'register'])->name('api.register');
